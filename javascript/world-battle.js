d151fc1896d7e32b9149a77cfc74189af92a4355
Ahmet Yagiz Aydin <ahmetyagiizaydin@gmail.com>
2024-01-11 14:23:31 +0300

    some carousel changes

--- javascript/world-battle.js
+++ javascript/world-battle.js
@@ -6,32 +6,38 @@ let images = ["images/card1.jpg", "images/card2.jpg", "images/card3.jpg", "image
 //slider functionality
 function handleChange() {
   document.slide.src = images[i];
-  if (i < images.length - 1) {
-  i++;
-  } else {
-  i = 0;
-  }
-  }
-  
-  // slider previous functionality
-  
-  function previous() {
+}
+
+// slider previous functionality
+function previous() {
   if (i === 0) {
-  i = images.length - 1;
+    i = images.length - 1;
   } else {
-  i--;
+    i--;
   }
-  document.slide.src = images[i];
-  }
-  
-  // slider next functionality
-  function next() {
+  handleChange();
+}
+
+// slider next functionality
+function next() {
   if(i === images.length - 1) {
-  i = 0;
+    i = 0;
   } else {
-  i++;
+    i++;
   }
-  document.slide.src = images[i];
-  }
-  
-  window.onload = handleChange;
+  handleChange();
+}
+
+// show image on hover
+function showImage(index) {
+  i = index;
+  handleChange();
+}
+
+// hide image on mouseout
+function hideImage() {
+  i = 0;
+  handleChange();
+}
+
+window.onload = handleChange;
